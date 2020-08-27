3bbf63efc2a82616cf6531c96ff799b499837140
irwinet <irwinestradatorres@gmail.com>
2020-08-27 18:27:56 -0500

    Implementation Details of Categories

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,5 +1,7 @@
 import 'package:app_flutter_news/src/models/category_model.dart';
 import 'package:app_flutter_news/src/services/news_service.dart';
+import 'package:app_flutter_news/src/theme/theme.dart';
+import 'package:app_flutter_news/src/widgets/list_news.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,11 +10,24 @@ class Tab2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final newsService = Provider.of<NewsService>(context);
+
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: <Widget>[
-            Expanded(child: _ListCategories()),
+            _ListCategories(),
+            if(!newsService.isLoading)
+              Expanded(
+                child: ListNews(newsService.getArticlesCategorySelected),
+              ),
+            
+            if(newsService.isLoading)
+              Expanded(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              )
           ],
         ),
       ),
@@ -27,23 +42,27 @@ class _ListCategories extends StatelessWidget {
 
     final categories=Provider.of<NewsService>(context).categories;
 
-    return ListView.builder(
-      physics: BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      itemCount: categories.length,
-      itemBuilder: (BuildContext context, int index){
-        final cName = categories[index].name;
-        return Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: <Widget>[
-              _CategoryButton(categories[index]),
-              SizedBox(height: 5,),
-              Text('${cName[0].toUpperCase()}${cName.substring(1)}')
-            ],
-          ),
-        );        
-      },
+    return Container(
+      width: double.infinity,
+      height: 80,
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index){
+          final cName = categories[index].name;
+          return Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                _CategoryButton(categories[index]),
+                SizedBox(height: 5,),
+                Text('${cName[0].toUpperCase()}${cName.substring(1)}')
+              ],
+            ),
+          );        
+        },
+      ),
     );
   }
 }
@@ -56,9 +75,13 @@ class _CategoryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    final newsService = Provider.of<NewsService>(context);
+    
     return GestureDetector(
       onTap: (){
-        print('${category.name}');
+        //print('${category.name}');        
+        newsService.selectedCategory = category.name;
       },
       child: Container(
         width: 40,
@@ -70,9 +93,10 @@ class _CategoryButton extends StatelessWidget {
         ),
         child: Icon(
           category.icon,
-          color: Colors.black54,
+          color: (category.name==newsService.selectedCategory)?myTheme.accentColor: Colors.black54,
         ),
       ),
     );
   }
 }
+
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -9,6 +9,9 @@ final _APIKEY='e4f49e9bfe8e4fd9b41669416c5c13d7';
 
 class NewsService with ChangeNotifier{
   List<Article> headlines = [];
+  String _selectedCategory = 'business';
+  bool _isLoading = false;
+
   List<Category> categories = [
     Category(FontAwesomeIcons.building,'business'),
     Category(FontAwesomeIcons.tv,'entertainment'),
@@ -18,10 +21,27 @@ class NewsService with ChangeNotifier{
     Category(FontAwesomeIcons.volleyballBall,'sports'),
     Category(FontAwesomeIcons.memory,'technology'),
   ];  
+
+  Map<String, List<Article>> categoryArticles = {};
   
   NewsService(){
     this.getTopHeadlines();
+    categories.forEach((element) {
+      this.categoryArticles[element.name] = new List();
+    });
   }
+
+  bool get isLoading => this._isLoading;
+
+  get selectedCategory => this._selectedCategory;
+  set selectedCategory (String value){
+    this._selectedCategory=value;
+    this._isLoading = true;
+    this.getArticlesByCategory(value);
+    notifyListeners();
+  }
+
+  List<Article> get getArticlesCategorySelected => this.categoryArticles[this.selectedCategory];
 
   getTopHeadlines() async{
     //print('Loading headlines...');
@@ -32,6 +52,26 @@ class NewsService with ChangeNotifier{
     final newsResponse = newsResponseFromJson(resp.body);
 
     this.headlines.addAll(newsResponse.articles);
+    notifyListeners();
+  }
+
+  getArticlesByCategory(String category) async{
+
+    if(this.categoryArticles[category].length>0){
+      this._isLoading = false;
+      notifyListeners();
+      return this.categoryArticles[category];
+    }
+
+    final url = '$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=us&category=$category';
+
+    final resp = await http.get(url);
+
+    final newsResponse = newsResponseFromJson(resp.body);
+
+    this.categoryArticles[category].addAll(newsResponse.articles);
+    
+    this._isLoading=false;
     notifyListeners();
   }
 
